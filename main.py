0d50ac736df08f628f64877faedf3b8192448c24
nngcai <nngcai@student.wethinkcode.co.za>
2020-11-11 12:31:38 +0200

    This is just a skeleton of how I'll be getting user input. I'm still currently learning how to implement the argparse module to get input and run program as a script just like LMS

---
+++ main.py
@@ -0,0 +1,25 @@
+import argparse
+import sys
+
+lst_flags = ["book_session", "volunteer", "available_slots", "help"]
+
+def get_flag():
+    flag = input("Are you booking a session or volunteering? (Type \'help\' for a list of available commands.) ").lower()
+    if flag not in lst_flags:
+        return get_flag()
+    
+    return flag
+
+
+def get_help():
+    
+    print("Here is a list of valid commands:\n")
+    print("\'help\' - Shows you this list.")
+    print("\'book_session\' - Book a session with an available clinician.")
+    print("\'volunteer\' - Volunteer as a clinician")
+
+def run_clinic():
+    flag = get_flag()
+
+if __name__ == "__main__":
+    run_clinic()
