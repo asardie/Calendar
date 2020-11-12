e3cb209b0f9f55903a07c69406206c63dcea55e4
asardie <asardie@student.wethinkcode.co.za>
2020-11-12 13:44:46 +0200

    work done

--- create_events.py
+++ create_events.py
@@ -3,11 +3,34 @@ import users
 import view_events
 import auth
 
+def get_time():
+    time = input('would you like to volunteer in the morning or afternoon (AM/pm)?: ')
+    while 1:
+        if time.lower() == 'am':
+            posible_times = [(datetime.time(hour=i//60, minute=(i%60))) for i in range(7*60, 12*60, 15)]
+            break
+        elif time.lower() == 'pm':
+            posible_times = [(datetime.time(hour=i//60, minute=(i%60))) for i in range(12*60, 17*60, 15)]
+            break
+        else:
+            time = input("please enter am or pm: ")
+
+    for i, time in enumerate(posible_times):
+        print(i, time, sep=' : ')
+
+    while 1:
+        t = input("enter a time id: (0,1,2,3,etc): ")
+        if t.isdigit():
+            return posible_times[int(t)]
+
+    
+
+    
+
 def add_to_clinic_calander():
     shared_service = auth.create_shared_service()
 
-    posible_times = [f"{i}:00" for i in range(7,16)]
-
+    time = get_time()
 
     event = {
     'summary': f"{' - '.join(['code_clinic', users.get_username()])}",
@@ -24,7 +47,5 @@ def add_to_clinic_calander():
 
     event = shared_service.events().insert(calendarId='primary', body=event).execute()
 
-posible_times = [(datetime.timedelta(hours=i)) for i in range(7, 16)]
 
-for p in posible_times:
-    print(p)
+print(get_time())
